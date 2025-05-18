544c90a347f72b42b6e54fd806e06bbbe9811bdd
Narsi Jangid <149683470+narsijangid@users.noreply.github.com>
2025-05-18 11:45:49 +0530

    Update Signup.js

--- frontend/src/pages/Signup.js
+++ frontend/src/pages/Signup.js
@@ -29,7 +29,7 @@ function Signup() {
             return handleError('name, email and password are required')
         }
         try {
-            const url = `http://localhost:8080/auth/signup`;
+            const url = `https://meme-generator-ai-1.onrender.com/auth/signup`;
             const response = await fetch(url, {
                 method: "POST",
                 headers: {
